a60d83804f9e489e427d014395a501033b897232
Abdul-Qayyum Olatunji <106759130+Qayyax@users.noreply.github.com>
2024-04-23 19:13:48 -0400

    updated bst.py

--- bst.py
+++ bst.py
@@ -10,6 +10,46 @@ class BinarySearchTree:
     def __init__(self):
         self.root = None
 
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+
+        temp = self.root
+        while temp:
+            if temp.value == new_node.value:
+                return False
+            if new_node.value > temp.value:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+            else:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+
+    def contains(self, value):
+        temp = self.root
+        # if self.root is None -> False would not trigger loop
+        # so it would return False
+        while temp:
+            if value > temp.value:
+                temp = temp.right
+            elif value < temp.value:
+                temp = temp.left
+            else:
+                return True
+        return False
+
 
 my_tree = BinarySearchTree()
-print(my_tree.root)
+my_tree.insert(2)
+print(my_tree.root.value)
+my_tree.insert(1)
+print(my_tree.root.left.value)
+my_tree.insert(3)
+print(my_tree.root.right.value)
+print(my_tree.contains(8))
